994fc45fda0aa3da9f180b71e76a164d655c8502
Emílio de Aguiar <85713266+emiliodaf@users.noreply.github.com>
2023-04-14 18:46:22 -0300

    first commit

---
+++ products.js
@@ -0,0 +1,112 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.png';
+import product7 from './assets/7.png';
+import product8 from './assets/8.png';
+import product9 from './assets/9.png';
+import product10 from './assets/10.png';
+import product11 from './assets/11.png';
+import product12 from './assets/12.png';
+
+/*  All the shop products was imported above here from an array of 12 objects   */
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Iphone",
+        price: 5989.00,
+        productImage: product1,
+        
+    },
+    {
+        id:2,
+        productName: "Notebook",
+        price: 989.00,
+        productImage: product2,
+
+    },
+    {
+        id:3,
+        productName: "Camera",
+        price: 589.00,
+        productImage: product3,
+
+           
+    },
+    {
+        id:4,
+        productName: "Shirt",
+        price: 279.00,
+        productImage: product4,
+        
+    },
+
+    {
+        id:5,
+        productName: "Led Light Strips",
+        price: 189.00,
+        productImage: product5,
+        
+    },
+
+    {
+        id:6,
+        productName: "Mousepad",
+        price: 79.00,
+        productImage: product6,
+        
+    },
+
+    {
+        id:7,
+        productName: "Computer",
+        price: 999.00,
+        productImage: product7,
+        
+    },
+
+    {
+        id:8,
+        productName: "Iphone",
+        price: 3389.00,
+        productImage: product8,
+        
+    },
+
+    {
+        id:9,
+        productName: "Nike Air",
+        price: 289.00,
+        productImage: product9,
+        
+    },
+
+    {
+        id:10,
+        productName: "Watch",
+        price: 98.00,
+        productImage: product10,
+        
+    },
+
+    {
+        id:11,
+        productName: "Watch",
+        price: 89.00,
+        productImage: product11,
+        
+    },
+
+    {
+        id:12,
+        productName: "Xbox Controller",
+        price: 189.00,
+        productImage: product12,
+        
+    },
+
+   
+]
